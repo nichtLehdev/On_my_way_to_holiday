8697892aa10ea45448a6344ed45bbcbab1bcb4d2
uzslo <kevinkern0606@gmail.com>
2022-11-04 23:25:35 +0100

    Main Game Prototyp begonnen

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool isGrounded = false;
+
+    public float speed = 5;
+    public float jump = 5;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal") + Input.acceleration.x;
+        transform.Translate(Vector2.right * speed * direction * Time.deltaTime);
+
+        Debug.Log(Input.acceleration.z);
+
+        if((Input.GetKeyDown(KeyCode.Space) && isGrounded == true) || isGrounded && Input.acceleration.z > 0)
+        {
+            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
