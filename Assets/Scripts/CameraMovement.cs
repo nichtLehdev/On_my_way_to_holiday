8d3471629e7e4b4eb1dc8b04bd1e86d73ac8f17c
Kevkef <kevinkern0606@gmail.com>
2022-11-09 09:25:01 +0100

    added camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+}
