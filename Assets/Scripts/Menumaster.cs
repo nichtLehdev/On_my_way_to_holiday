f9845e2d97d95f0ecbeef1b3380d5ab66f5bbd0a
Lars <lars@lehdev.de>
2023-01-29 12:44:35 +0100

    Added Internet and Username Check

--- Assets/Scripts/Menumaster.cs
+++ Assets/Scripts/Menumaster.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using GlobalstatsIO;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Menumaster : MonoBehaviour
 {
@@ -10,24 +12,79 @@ public class Menumaster : MonoBehaviour
     public GameObject MainWindow;
     public GameObject UsernameWindow;
     public TMP_InputField InputField;
+    public GameObject InternetError;
+
+    private GlobalstatsIO.Leaderboard leaderboard;
 
     private void Start()
     {
-        
+        showNoInternet();
         MainWindow.SetActive(true);
         RankingWindow.SetActive(false);
         UsernameWindow.SetActive(false);
         Debug.Log(PlayerPrefs.GetString("username"));
-        if ((PlayerPrefs.GetString("username") == null || PlayerPrefs.GetString("username") == "") && (PlayerPrefs.GetString("skip_name") == null || PlayerPrefs.GetString("skip_name") == ""))
+        if (Application.internetReachability != NetworkReachability.NotReachable)
         {
-            UsernameWindow.SetActive(true);
+            if ((PlayerPrefs.GetString("username") == null || PlayerPrefs.GetString("username") == "") && (PlayerPrefs.GetString("skip_name") == null || PlayerPrefs.GetString("skip_name") == ""))
+            {
+                UsernameWindow.SetActive(true);
+            }
         }
+       
+    }
+
+    private void Update()
+    {
+        showNoInternet();
+    }
+
+    public void getLeaderboard()
+    {
+        var gs = new GlobalstatsIOClient(config.getId(), config.getSecret());
+        string gtd = "score";
+        int limit = 100;
+
+        StartCoroutine(gs.GetLeaderboard(gtd, limit, LeaderboardCallback));
+    }
+
+    private void LeaderboardCallback(GlobalstatsIO.Leaderboard leaderboard)
+    {
+        if (leaderboard != null)
+        {
+            this.leaderboard = leaderboard;
+        }
+        else
+        {
+
+        }
+    }
+
+    private bool nameIsUsed(string name)
+    {
+        getLeaderboard();
+        if(leaderboard != null)
+        {
+            foreach (LeaderboardValue item in this.leaderboard.data)
+            {
+                if(item.name == name) return true;
+            }
+        }
+        return false;
     }
 
     public void setName()
     {
-        PlayerPrefs.SetString("username", InputField.text);
-        UsernameWindow.SetActive(false);
+        var gs = new GlobalstatsIOClient(config.getId(), config.getSecret());
+        string userName = InputField.text;
+        if (userName != null && userName != "" && !nameIsUsed(userName)) {
+            PlayerPrefs.SetString("username", userName);
+            Dictionary<string, string> values = new Dictionary<string, string>();
+            values.Add("score", "0");
+            StartCoroutine(gs.Share(values, "", userName));
+            UsernameWindow.SetActive(false);
+        }
+
+        
     }
 
     public void setSkip()
@@ -43,9 +100,25 @@ public class Menumaster : MonoBehaviour
 
     public void showRanking()
     {
-        MainWindow.SetActive(false);
-        RankingWindow.SetActive(true);
-        PrefabManager.showLeaderboard();
+        if (!InternetError.activeSelf)
+        {
+            MainWindow.SetActive(false);
+            RankingWindow.SetActive(true);
+            PrefabManager.showLeaderboard();
+        }
+        
+    }
+
+    private void showNoInternet()
+    {
+        if(Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            InternetError.SetActive(true);
+        }
+        else
+        {
+            InternetError.SetActive(false);
+        }
     }
 
     public void hideRanking()
